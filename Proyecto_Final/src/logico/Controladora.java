bfff9d9f1265fab1043e079ee917af90fc3a97f9
Rachi <FRGW0001@ce.pucmm.edu.do>
2025-07-18 00:03:49 -0400

    Funciones CRUD.

--- Proyecto_Final/src/logico/Controladora.java
+++ Proyecto_Final/src/logico/Controladora.java
@@ -10,11 +10,11 @@ public class Controladora {
 	public static ArrayList<Estadistica> misEstadisticas;
 	public static ArrayList<Juego> misJuegos;
 	public static ArrayList<EstadisticaJuego> misEstadisticaJuegos;
-	public static int codEquipo = 0;
-	public static int codCiudad = 0;
-	public static int codJugador = 0;
-	public static int codEstadistica = 0;
-	public static int codJuego = 0;
+	public static int codEquipo = 1;
+	public static int codCiudad = 1;
+	public static int codJugador = 1;
+	public static int codEstadistica = 1;
+	public static int codJuego = 1;
 	
 	
 	public static void main(String[] args) {
@@ -110,5 +110,156 @@ public class Controladora {
 		codJuego += 1;
 		return codJuego;
 	}
+	
+	public void insertarEquipo(Equipo equipo) {
+		misEquipos.add(equipo);
+		codEquipo++;
+	}
+	
+	public void insertarCiudad(Ciudad ciudad) {
+		misCiudades.add(ciudad);
+		codCiudad++;
+	}
+	
+	public void insertarJugador(Jugador jugador) {
+		misJugadores.add(jugador);
+		codJugador++;
+	}
+	
+	public void insertarEstadistica(Estadistica estadistica) {
+		misestadisticas.add(estadistica);
+		codEstadistica++;
+	}
+	
+	public void insertarJuego(Juego juego) {
+		misJuegos.add(juego);
+		codJuego++;
+	}
+	
+	public void updateEquipo(Equipo equipo, int ind) {
+		misEquipos.set(ind, equipo);
+		
+		
+	}
+	
+	public void updateCiudad(Ciudad ciudad, int ind) {
+		misCiudades.set(ind, ciudad);
+		
+		
+	}
+	public void updateJugador (Jugador jugador, int ind) {
+		misJugadores.set(ind, jugador);
+		
+		
+	}
+	public void updateEstadistica(Estadistica estadistica, int ind) {
+		misEstadisticas.set(ind, stadistica);
+		
+		
+	}
+	public void updateJuego(Juego juego, int ind) {
+		misJuegos.set(ind, juego);
+		
+		
+	}
+	public void deleteEquipo (String codEquipo) {
+		Equipo equipo = searchEquipoByCod(codEquipo);
+		misEquipos.remove(equipo);
+	}
+	
+	public void deleteCiudad (String codCiudad) {
+		ciudad ciudad = searchCiudadByCod(codCiudad);
+		misCiudades.remove(ciudad);
+	}
+	
+	public void deleteJugador (String codJugador) {
+		Jugador jugador = searchJugadorByCod(codJugador);
+		misJugadores.remove(jugador);
+	}
+	
+	public void deleteEstadistica (String codEstadistica) {
+		Estadistica estadistica = searchEstadisticaByCod(codEstadistica);
+		misEstadisticas.remove(estadistica);
+	}
+	
+	public void deleteJuego (String codJuego) {
+		Juego juego = searchJuegoByCod(codJuego);
+		misJuegos.remove(juego);
+	}
+	
+	 public Equipo searchEquipoByCod(int codEquipo) {
+	        Equipo equipo = null;
+	        boolean encontrado = false;
+	        int i = 0;
+
+	        while (!encontrado && i < misEquipos.size()) {
+	            if (misEquipos.get(i).getCodEquipo() == codEquipo) {
+	                equipo = misEquipos.get(i);
+	                encontrado = true;
+	            }
+	            i++;
+	        }
+	        return equipo;
+	    }
+	 
+	 public Jugador searchJugadorByCod(int codJugador) {
+	        Jugador jugador = null;
+	        boolean encontrado = false;
+	        int i = 0;
+
+	        while (!encontrado && i < misJugadores.size()) {
+	            if (misJugadores.get(i).getCodJugador() == codJugador) {
+	                encontrado = true;
+	                jugador = misJugadores.get(i);
+	            }
+	            i++;
+	        }
+	        return jugador;
+	    }
+	
+	 public Juego searchJuegoByCod(int codJuego) {
+	        Juego juego = null;
+	        boolean encontrado = false;
+	        int i = 0;
+
+	        while (!encontrado && i < misJuegos.size()) {
+	            if (misJuegos.get(i).getCodJuego() == codJuego) {
+	                encontrado = true;
+	                juego = misJuegos.get(i);
+	            }
+	            i++;
+	        }
+	        return juego;
+	    }
+	 
+	 public Ciudad searchCiudadByCod(int codCiudad) {
+	        Ciudad ciudad = null;
+	        boolean encontrado = false;
+	        int i = 0;
+
+	        while (!encontrado && i < misCiudades.size()) {
+	            if (misCiudades.get(i).getCodCiudad() == codCiudad) {
+	                encontrado = true;
+	                ciudad = misCiudades.get(i);
+	            }
+	            i++;
+	        }
+	        return ciudad;
+	    }
+	 
+	 public Estadistica searchEstadisticaByCod(int codEstadistica) {
+	        Estadistica estadistica = null;
+	        boolean encontrado = false;
+	        int i = 0;
+
+	        while (!encontrado && i < misEstadisticas.size()) {
+	            if (misEstadisticas.get(i).getCodEstadistica() == codEstadistica) {
+	                encontrado = true;
+	                estadistica = misEstadisticas.get(i);
+	            }
+	            i++;
+	        }
+	        return estadistica;
+	    }
 	
 }
